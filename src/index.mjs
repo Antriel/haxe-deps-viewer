f0fe6fe1a92226c8b9f7a354f3de18118cdce3b8
Antriel <Antriel@users.noreply.github.com>
2024-12-10 12:56:48 +0100

    handle pasting the file too

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,13 @@
 import { showNew } from "./renderer.mjs";
 
+function handleText(text, filename) {
+    if (text?.includes('.hx')) {
+        hello.remove();
+        const isDependants = filename ? filename.indexOf('dependants') >= 0 : false;
+        showNew(text, isDependants);
+    }
+}
+
 const el = document.getElementById('container');
 const hello = document.getElementById('hello');
 el.ondragover = () => { hello.classList.add('dragging'); return false; };
@@ -10,16 +18,21 @@ el.ondrop = e => {
     hello.classList.remove('dragging');
     for (const file of e.dataTransfer.files) {
         const reader = new FileReader();
-        reader.onload = event => {
-            const text = event.target.result;
-            if (text.includes('.hx')) {
-                hello.remove();
-                const isDependants = file.name.indexOf('dependants') >= 0;
-                showNew(text, isDependants);
-            }
-        }
+        reader.onload = event => handleText(event.target.result, file.name);
         reader.readAsText(file);
         return false;
     }
     return false;
 }
+document.addEventListener("paste", async (event) => {
+    const items = event.clipboardData.items;
+    for (const item of items) if (item.kind === "file") {
+        const file = item.getAsFile();
+        if (file) {
+            handleText(await file.text(), file.name);
+            return;
+        }
+    }
+    // Check for straight text.
+    handleText(event.clipboardData.getData("text"));
+});
